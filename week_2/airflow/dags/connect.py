b33171ae2c9d6877bf822ae459a0c22f4d5892ab
paulonye <nwosupaul141@gmail.com>
2023-01-23 14:09:29 +0000

    Added DB connection file

---
+++ week_2/airflow/dags/connect.py
@@ -0,0 +1,22 @@
+from sqlalchemy import create_engine
+import os
+
+user = os.environ.get('PGUSER', 'main')
+password = os.environ.get('PGPASS', 'root')
+port = 5432
+host = os.environ.get('HOST', '********')
+db = os.environ.get('DB', 'ny_taxi')
+
+def get_connection():
+
+    postgres_url = f'postgresql://{user}:{password}@{host}:{port}/{db}'
+
+    engine  = create_engine(postgres_url)
+
+    return engine
+
+
+if __name__ == '__main__':
+    con = get_connection()
+    print('Connecton Successful')
+    con.connect().close()
